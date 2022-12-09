368cf6e8164db6cad71956ef4695316162597f9d
lem224 <lem224@lehigh.edu>
2022-12-09 16:07:06 -0500

    Clean up

--- Modified-Model-Results.R
+++ Modified-Model-Results.R
@@ -3,38 +3,38 @@ source("Voter-model-simulations.R")
 ## Minimum dimension 2
 
 mean(VM(2,1,10000)) # 0.5
-mean(V4_time(2,1,10000)) # 1
+mean(myModified_time(2,1,10000)) # 1
 # 0.5*0.5*4 = 1
 
 # mean(VM_complete(2,2,10000)) # 3.02
-# mean(V4_complete(2,2,10000)) # 17.6967
+# mean(myModified_complete(2,2,10000)) # 17.6967
 
 mean(VM(2,2,10000)) # 5.9
-mean(V4_time(2,2,1000)) # 26.1256
+mean(myModified_time(2,2,1000)) # 26.1256
 # 5.9*5.9*(3/4) = 26.1075
 
 # mean(VM_complete(2,3,10000)) # 7.4383
-# mean(V4_complete(2,3,1000)) # 102.37
+# mean(myModified_complete(2,3,1000)) # 102.37
 mean(VM(2,3,10000)) # 18.1
-mean(V4_time(3,2,10000)) # 160
+mean(myModified_time(3,2,10000)) # 160
 #18.1*18.1*(1/2) = 163.805
 
 (18.1*18.1/2)-18.1
 
 mean(VM(2,4,10000)) # 42.4
-mean(V4_time(2,4,5000)) # 795
+mean(myModified_time(2,4,5000)) # 795
 # 42.4*42.4/2 = 898.88
 
 42.4*42.4/2 - 42.4
 
 mean(VM(2,5,10000)) # 78.4233
-mean(V4_time(2,5,5000)) # 3681.061
+mean(myModified_time(2,5,5000)) # 3681.061
 # 78.4*78.4/2 = 3073.28
 
 78.4*78.4/2 - 78.4
 
 mean(VM(2,6,10000)) # 133.4
-mean(V4_time(2,6,300)) # 16897.84
+mean(myModified_time(2,6,300)) # 16897.84
 # 133.4*133.4 = 17795.6
 
 (133.4*133.4) - 133.4
@@ -42,43 +42,43 @@ mean(V4_time(2,6,300)) # 16897.84
 ## Minimum dimension 3
 
 # mean(VM_complete(3,3,10000)) # 47.3622
-# mean(V4_complete(3,3,1000)) # 1370.486
+# mean(myModified_complete(3,3,1000)) # 1370.486
 
 mean(VM(3,3,10000)) # 50.9
-mean(V4_time(3,3,1000)) # 1263.61
+mean(myModified_time(3,3,1000)) # 1263.61
 # 51*51*(1/2) - 51= 1249.5
 
 51*51/2 - 51 # 1249.5
 
 mean(VM(3,4,20000)) # 106.1
-mean(V4_time(3,4,500)) # 9666.416
+mean(myModified_time(3,4,500)) # 9666.416
 # 106*106*(2/2) - 106*12 = 11,130
 
 106*106*(2/2) - 106*(4^2)
 
 mean(VM(3,5,10000)) # 186.56
-mean(V4_time(3,5,300)) # 83756.84
+mean(myModified_time(3,5,300)) # 83756.84
 # 186.6*186.6*(6/2) = 104,458
 
 186.6*186.6*(6/2) - 186.56*(5^3)
 
 mean(VM(3,6,10000)) # 302.4223
-mean(V4_time(3,6,500)) # 722845.9
+mean(myModified_time(3,6,500)) # 722845.9
 # 302.42*302.42*(24/2) = 1,097,494.28
 
 302.42*302.42*(24/2)-(302)*(6^(4))
 
 # mean(VM_complete(3,7,10000))
 mean(VM(3,7,10000)) # 454.0221
-mean(V4_time(3,7,750)) # 6100133
+mean(myModified_time(3,7,750)) # 6100133
 
 ## Minimum dimension 4
 
 mean(VM(4,4,10000)) # 206.976
-mean(V4_time(4,4,1000)) # 144260
+mean(myModified_time(4,4,1000)) # 144260
 
 mean(VM(4,5,10000)) # 364.2978
-mean(V4_time(4,5,1000)) # 2272649
+mean(myModified_time(4,5,1000)) # 2272649
 
 mean(VM(4,6,10000)) # 560.2132
 
